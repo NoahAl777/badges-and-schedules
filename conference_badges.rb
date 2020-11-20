697b63a47bff1e622be2b6429ae2b242da936820
noah <crashlive112@gmail.com>
2020-11-20 17:19:44 -0500

    commit tree 1818ea361f070638ef9e4a105b27e14d4a461d49

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(array)
+"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |name|
+    "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name, room|
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end
